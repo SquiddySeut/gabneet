b6ded5cfdeb60f7a05e155cfc80ead5e91b5c0e6
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2019-01-24 09:40:56 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,13 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+    int cs=0;
+    if (message.content==="I wonder if Gabriel will come..."){
+        //message.reply("how long do you want us to talk?")
+        message.channel.send("I want games............");
+        message.channel.send("Anyone have foood?");
+        cs=1;
+    }    
 });
 
 // THIS  MUST  BE  THIS  WAY
