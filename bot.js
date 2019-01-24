b973ac93000cc2352b4f04bb592af7c476f2f8de
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2019-01-24 10:12:26 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -12,10 +14,22 @@ client.on('message', message => {
     int cs=0;
     if (message.content==="I wonder if Gabriel will come..."){
         //message.reply("how long do you want us to talk?")
-        message.channel.send("I want games............");
+        message.channel.send("I want my computer games............");
         message.channel.send("Anyone have foood?");
         //cs=1;
     }    
+    if(message.content==="Gabriel, you want this メロンパン™?"){
+           cs=getRandomInt(2);
+        if(cs===0){
+            message.channel.send("hAI YeS PLAESE GIMMe");
+        }
+        else if(cs===1){
+            message.channel.send("no you probably poisoned it satan");
+        }
+        else{
+            message.channel.send("mlep nep e");
+        }
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
